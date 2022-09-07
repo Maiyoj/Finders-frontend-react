e231e1fc2db544c6dba78707d5782e16cf7575e8
Maiyoj <valentinemaiyo1995@gmail.com>
2022-09-07 10:28:08 +0300

    [update] navbar logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar(){
     return(
         <>
         <nav className="flex justify-around p-8 bg-blue-900 h-24 text-white " >
-            <logo>Games Finder</logo>
+            <h1>Games Finder</h1>
             <div className="px-6">
             <Link to={"/"} exact="true">Home</Link>
             <Link to={"/about"} exact="true">About</Link>
