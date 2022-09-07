8569898aad00aff5b0382eb10c9cac4a8e001a9b
Maiyoj <valentinemaiyo1995@gmail.com>
2022-09-07 19:25:14 +0300

    [update] single card styling

--- src/components/SingleGame.js
+++ src/components/SingleGame.js
@@ -1,9 +1,9 @@
 function SingleGame({name, description}){
     return(
     <div className="flex justify-center">
-        <div className="block p-6 rounded-lg shadow-lg bg-white max-w-96 h-40">
+        <div className="block p-6 rounded-lg shadow-lg bg-white max-w-96 h-60">
              <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">{name}</h5>
-             <p className="text-gray-700 text-base mb-4">{description}</p>
+             <p className="text-gray-700 text-base mb-4 " >{description}</p>
                      <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Delete</button>
                      <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Edit</button>
                      </div>
