5ca7001faa8f4e5c196ceaa7334796ffec9e26af
Maiyoj <valentinemaiyo1995@gmail.com>
2022-09-08 20:26:43 +0300

    [add] go back button

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,7 @@ import SingleReview from "./SingleReview"
 import {useState, useEffect} from "react"
 import { useParams } from "react-router-dom";
 import EditGame from "./EditGame";
+import { Link } from "react-router-dom";
 
 
 
@@ -27,9 +28,8 @@ function Game(){
                      <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2"></h5>
                       <p  className="text-gray-700 text-base mb-4">{review.body}</p>
                       </div>
-                       </div>
-                    )
-                })}
+                       </div>   )})}
+                       <Link to={`/games`} className=" inline-block px-6 py-2.5 bg-blue-600 mt-10 ml-20 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out place-content-end ">Go Back</Link>
 
         </>   
     )
