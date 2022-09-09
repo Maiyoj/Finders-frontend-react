b8a70cfa5bba38f820be4e1ebca98fca0ea4d189
Maiyoj <valentinemaiyo1995@gmail.com>
2022-09-09 15:15:22 +0300

    [update] home paragraph

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,11 @@ function Home(){
   return(
     <>
   <img src="https://imgs.callofduty.com/content/dam/atvi/callofduty/cod-touchui/mw2/meta-images/reveal/mw2-reveal-meta-share.jpg" className=" bg-cover" alt="Gmae review" />
-     <p ></p>
+     <div className="absolute text-5xl  text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+     <p >Get The best of Game</p>
+     <p >Check out available Games</p>
+     <p >Recommend a game </p>
+     </div>
 
     </>
         
